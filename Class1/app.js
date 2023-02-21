64c600d408d86dd345fe54877dfb35aceca5343e
Peter <113168584+prawstho@users.noreply.github.com>
2023-02-21 15:12:41 -0330

    Class 1 completed

---
+++ Class1/app.js
@@ -0,0 +1,10 @@
+// this is a comment
+
+/*
+this is also a comment
+*/
+
+alert('JavaScript from a js file.');
+
+// in the browser window, right click and inspect
+console.log("logging to browser console");
